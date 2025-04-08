4eb3fcf4d73581b334dcc5c496c70f0c6e9e8324
Goodness Adewuyi <adewuyigoodness1@gmail.com>
2025-04-08 13:14:49 +0100

    Initial Commit.

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="text-center text-5xl pt-20 text-teal-900 " >
+        Mint Bot Goes Here.
+      </div>
+    </>
+  )
+}
+
+export default App
