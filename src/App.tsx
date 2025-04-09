3bbc8f2d2291fc9ba2d1fd7bca5352764e57e43a
Toluwani <dawodutoluwani@gmail.com>
2025-04-09 14:49:14 +0100

    home page done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css'
+import Home from './Home';
 
 function App() {
-
   return (
-    <>
-      <div className="text-center text-5xl pt-20 text-teal-900 " >
-        Mint Bot Goes Here.
-      </div>
-    </>
+    <Home />
   )
 }
 
